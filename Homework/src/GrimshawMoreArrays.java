7e0479b1ccfb52c75c94d46af73e3f616f46be2f
danielgrimshaw <danielgrimshaw42@gmail.com>
2014-11-12 22:01:21 -0800

    Finished MoreArrays homework.

--- Homework/src/GrimshawMoreArrays.java
+++ Homework/src/GrimshawMoreArrays.java
@@ -5,16 +5,16 @@
  *       int	x	=	1;
  *       int[]	a	=	new	int[2];
  *       mystery(x,	a);
- *       System.out.println(x	+	“	“	+	Arrays.toString(a));
+ *       System.out.println(x	+			+	Arrays.toString(a));
  *       x--;
  *       a[1]	=	a.length;
  *       mystery(x,	a);
- *       System.out.println(x	+	“	“	+	Arrays.toString(a));
+ *       System.out.println(x	+			+	Arrays.toString(a));
  *    }
  *    public	static	void	mystery(int	x,	int[]	list) {
  *       list[x]++;
  *       x++
- *       System.out.println(x	+	“	“	+	Arrays.toString(list));
+ *       System.out.println(x	+			+	Arrays.toString(list));
  *    }
  * }
  * Output:
@@ -22,29 +22,9 @@
  * _1 [0,1]____________________________________
  * _1 [1,2]____________________________________
  * _0 [1,2]____________________________________
- * Note: Complete	the problems in	one	program	file	named	LastnameMoreArrays.java	and	call	any	
- * requested	method	from	the main	method.	
- * Note: For	each	problem	in	your	program,	add	a	comment	at	the	beginning	stating	the	number	of	the	
- * problem.
- *
- * 3. Write	a	method	called	isSorted	that	accepts	an	array	of	real	numbers	as	a	parameter	and	returns	true	
- * if	the	list	is	in	sorted	(nondecreasing)	order	and	false	otherwise.	For	example,	if	arrays	named	list1	
- * and	list2	store	{16.1,	12.3,	22.2,	14.4}	and	{1.5,	4.3,	7.0,	19.5,	25.1,	46.2}	respectively,	the	calls	
- * isSorted(list1)	and	isSorted(list2)	should	return	false	and	true	respectively.	Assume	that	the	array	has	
- * at	least	one	element.	A	one-element	array	is	considered	sorted.	From	main,	print	the	array	contents	
- * and	print	whether	the	array	is	sorted.
- *
- * 4. Write	a	method	called	isUnique	that	accepts	an	array	of	integers	as	a	parameter	and	returns	a	boolean
- * value	indicating	whether	or	not	the	values	in	the	array	are	unique	(true	for	yes,	false	for	no).	The	
- * values	in	the	list	are	considered	unique	if	there	is	no	pair	of	values	that	are	equal.	For	example,	if	
- * passed	an	array	containing	[3,	8,	12,	2,	9,	17,	43, -8,	46],	your	method	should	return	true,	but	if	passed	
- * [4,	7,	3,	9,	12,	-47,	3,	74],	your	method	should	return	false	because	the	value	3	appears	true.
- * 5. Write	a	method	of	your	choosing	which	uses	at	least	one	array.	Within	the	method	also	use	at	least	
- * one	of	the	following	structures:	if	/	else,	for,	for-each,	while.	Provide	a	comment	stating	what	your	
- * method	does.
  */
- 
-import java.util.*;
+
+import java.util.Arrays;
 
 public class GrimshawMoreArrays {
    public static void main(String[] args) {
@@ -56,8 +36,19 @@ public class GrimshawMoreArrays {
       if (isPalindrome(palindrome))
          System.out.println(Arrays.toString(palindrome) + " is a palindrome");
          
-      if isSorted({16.1,	12.3,	22.2,	14.4})
-         System.out.println(Arrays.toString({16.1,	12.3,	22.2,	14.4})
+      double [] list1 = {16.1,       12.3,   22.2,   14.4};
+      double [] list2 = {1.5,   4.3,    7.0,    19.5,   25.1,   46.2};
+      if (isSorted(list1))
+         System.out.println(Arrays.toString(list1) + " is a sorted array");
+      else
+         System.out.println(Arrays.toString(list1) + " is not a sorted array");
+      if (isSorted(list2))
+         System.out.println(Arrays.toString(list2) + " is a sorted array");
+      else
+         System.out.println(Arrays.toString(list2) + " is not a sorted array");
+      int [] test1 = {3,     8,      12,     2,      9,      17,     43, -8, 46};
+      int [] test2 = {4,  7,      3,      9,      12,     -47,    3,      74};
+      System.out.println(isUnique(test1)+"\n"+isUnique(test2));
    }
    
    public static double avgLength(String [] strings) {
@@ -80,4 +71,24 @@ public class GrimshawMoreArrays {
       for (int i = 1; i < doubles.length; i++)
          if (doubles[i-1] > doubles[i]) return false;
       return true;
+   }
+
+   public static boolean isUnique(int [] nums) {
+      for (int i = 0; i < nums.length-1; i++)
+         for (int j = i+1; j < nums.length; j++)
+            if (nums[i] == nums[j]) return false;
+      return true;
+   }
+
+   public static int getMax4D(int [][][][] nums) {
+      //finds the larges single value in a four dimensional array
+      int max = Integer.MIN_VALUE;
+      for (int [][][] threeD : nums)
+         for (int [][] twoD : threeD)
+            for (int [] oneD : twoD)
+               for (int i : oneD)
+                  if (i > max)
+                     max = i;
+      return max;
+   }
 }
