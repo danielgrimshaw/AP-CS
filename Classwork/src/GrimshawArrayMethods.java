c2423ca22232a9436b947088a9d2c9839cb1d7e9
Daniel <danielgrimshaw42@gmail.com>
2014-11-20 10:08:09 -0800

    Classwork 11/20/14

--- Classwork/src/GrimshawArrayMethods.java
+++ Classwork/src/GrimshawArrayMethods.java
@@ -2,39 +2,61 @@ import java.util.Arrays;
 
 public class GrimshawArrayMethods {
    public static void main(String[] args) {
-      int [][] arr = new int[4][4];
+      int [] findMinTest = {16, 12, 25, 44};
+      System.out.println("The minimum of "+Arrays.toString(findMinTest)+" is "+findMin(findMinTest));
       
-      for (int [] i : arr)
-         System.out.println(Arrays.toString(i));
-      System.out.println();
-      setValues(arr);
-      for (int [] i : arr)
-         System.out.println(Arrays.toString(i));
-         
-      int [][] arr2 = {{1,4,6},{7,2,5},{9,8,3}};
-      System.out.println();
-      for (int [] i : arr2)
-         System.out.println(Arrays.toString(i));
-      System.out.println();
-      reverseValues(arr2);
-      for (int [] i : arr2)
-         System.out.println(Arrays.toString(i));
+      int [] countTest = {3, 5, 2, 1, 92, 38, 3, 14, 5, 73, 5};
+      System.out.println("There are "+count(countTest,3)+" 3's in "+Arrays.toString(findMinTest)+".");
+      
+      int [] stretchTest = {18, 7, 4, 24, 11};
+      System.out.println("Before stretch: "+Arrays.toString(stretchTest)+"\nAfter stretch: "+Arrays.toString(stretch(stretchTest)));
+      
+      int[] list1 = {1, 6, 2, 1, 4, 1, 2, 3, 8}; 
+      int[] list2 = {1, 2, 3}; 
+      System.out.println(Arrays.toString(list1)+" contains "+Arrays.toString(list2)+" is "+contains(list1, list2));
+   }
+   public static int findMin(int [] vals) {
+      int min = Integer.MAX_VALUE;
+      for (int i : vals)
+         if (i < min)
+            min = i;
+      return min;
    }
    
-   public static void setValues(int [][] nums) {
-      for (int i = 0; i < nums[2].length; i++)
-         nums[2][i] = 3;
-      for (int i = 0; i < nums.length; i++)
-         nums[i][3] = 4;
+   public static int count(int [] vals, int pattern) {
+      int cnt = 0;
+      for (int i : vals)
+         if (i == pattern)
+            cnt++;
+      return cnt;
    }
    
-   public static void reverseValues(int [][] nums) {
-      for (int i = 0; i < nums.length; i++){
-          for (int j = 0; j < nums[i].length/2; j++) {
-             int tmp = nums[i][j];
-             nums[i][j] = nums[i][nums[i].length-j-1];
-             nums[i][nums[i].length-j-1] = tmp;
-          }
+   public static int [] stretch(int [] original) {
+      int [] stretched = new int [original.length*2];
+      for (int i = 0; i < stretched.length; i += 2) {
+         int val = original[i/2]/2;
+         int val2;
+         if (original[i/2]%2 != 0)
+            val2 = val+1;
+         else
+            val2 = val;
+         stretched[i] = val2;
+         stretched[i+1] = val;
       }
+      return stretched;
    }
+   
+   public static boolean contains(int [] source, int [] pattern) {
+      for (int i = 0; i < source.length; i++) {
+         int matches = 0;
+         for (int j = 0; j < pattern.length; j++) {
+            if (i+j >= source.length) return false;
+            if (source[i+j] == pattern[j])
+               matches++;
+         }
+         if (matches == pattern.length) return true;
+      }
+      return false;
+   }
+               
 }
