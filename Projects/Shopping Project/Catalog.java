3b9471121c440bef8be77db98a6605fd8d85721c
Daniel <danielgrimshaw42@gmail.com>
2015-02-03 10:37:16 -0800

    commit

---
+++ Projects/Shopping Project/Catalog.java
@@ -0,0 +1,10 @@
+public class Catalog {
+   private String name;
+   private List<Item> items;
+   
+   public Catalog(String name) {
+      this.name = name;
+      this.items = new ArrayList<Item>;
+   }
+   
+   public 
--- Projects/Shopping Project/Item.java
+++ Projects/Shopping Project/Item.java
@@ -9,16 +9,35 @@ public class Item {
    
    public Item(String name, double price, int bulk_quantity, double bulk_price) {
       this.name = name;
+      this.bulk = new double [2];
       this.setPrice(price);
       this.setBulk(bulk_quantity, bulk_price);
    }
    
    private void setPrice(double price) {
-      if (price >= 0) {
+      if (price >= 0)
          this.price = price;
       else
          throw new IllegalArgumentException("Price is negative");
    }
    
    private void setBulk(int bulk_quantity, double bulk_price) {
-   
+      if (bulk_quantity < 0 || bulk_price < 0)
+         throw new IllegalArgumentException("Negative bulk quantity or price");
+      else {
+         this.bulk[0] = bulk_quantity;
+         this.bulk[1] = bulk_price;
+      }
+   }
+      
+   public double priceFor(int quantity) {
+      return (this.quantity/this.bulk[0] * this.bulk[1]) + (this.quantity%this.bulk[0] * this.price);
+   }
+         
+   public String toString() {
+      String ret = this.name+ ", " + this.price;
+      if (this.bulk[0] != 1)
+         ret += "("+this.bulk[0]+" for "+this.bulk[1]+")";
+      return ret;
+   }
+}
