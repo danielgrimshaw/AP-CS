c62e57422f369572c4bb1449b25739528c77d480
Daniel <danielgrimshaw42@gmail.com>
2015-02-06 10:14:16 -0800

    Finished Magpie 4

--- Classwork/GrimshawMagpieFour/Magpie4.java
+++ Classwork/GrimshawMagpieFour/Magpie4.java
@@ -53,8 +53,14 @@ public class Magpie4
 		{
 			response = transformIWantToStatement(statement);
 		}
-      
-
+      else if (findKeyword(statement, "I want", 0) >= 0)
+      {
+         response = transformIWantStatement(statement);
+      }
+      else if (findKeyword(statement, "I", 0) == 0 && findKeyword(statement, "you", 0) >= 0)
+      {
+         response = transformIYouStatement(statement);
+      }
 		else
 		{
 			// Look for a two word (you <something> me)
@@ -95,6 +101,20 @@ public class Magpie4
 		String restOfStatement = statement.substring(psn + 9).trim();
 		return "What would it mean to " + restOfStatement + "?";
 	}
+   
+   private String transformIWantStatement(String statement)
+   {
+      statement = statement.trim();
+      String lastChar = statement.substring(statement.length() - 1);
+      if (lastChar.equals("."))
+      {
+         statement = statement.substring(0, statement.length() - 1);
+      }
+      
+      int psn = findKeyword(statement, "I want", 0);
+      String restOfStatement = statement.substring(psn+6).trim();
+      return "Would you really be happy if you had "+restOfStatement+"?";
+   }
 
 	
 	
@@ -123,7 +143,20 @@ public class Magpie4
 		return "What makes you think that I " + restOfStatement + " you?";
 	}
 	
-	
+   private String transformIYouStatement(String statement)
+   {
+      statement = statement.trim();
+		String lastChar = statement.substring(statement
+				.length() - 1);
+		if (lastChar.equals("."))
+		{
+			statement = statement.substring(0, statement.length() - 1);
+		}
+      
+      int psnOfYou = findKeyword(statement, "you", 1);
+      String restOfStatement = statement.substring(2,psnOfYou-1);
+      return "Why do you "+restOfStatement+" me?";
+	}
 
 	
 	
