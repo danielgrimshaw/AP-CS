2d7bdeb6aa6a34875a92d82c240f1dee4c00324d
Daniel <danielgrimshaw42@gmail.com>
2015-03-03 10:23:46 -0800

    Did in class problems

--- Classwork/Temp.java
+++ Classwork/Temp.java
@@ -1,17 +1,46 @@
 import java.util.Arrays;
 
+import java.util.Arrays;
+
 public class Temp {
    public static void main(String []  args) {
-      mystery2(100);
+      int [] primes = {2,3,5,7,11,13,17,19,23};
+      backwardDisplay(primes);
+      System.out.println("\n"+Arrays.toString(primes));
+      
    }
    
-   public static void mystery2(int n) {
-      if (n <= 1)
-         System.out.print(n);
-      else {
-         mystery2(n / 2);
-         System.out.print(", " + n);
+   public static void forwardDisplay(int [] primes) {
+      if (primes.length > 0) {
+         System.out.print(primes[0] + "  ");
+         
+         int [] newPrimes = new int [primes.length-1];
+         for (int i = 1; i < primes.length; i++)
+            newPrimes[i-1] = primes[i];
+      
+         forwardDisplay(newPrimes);
+      }
+   }
+         
+   public static void backwardDisplay(int [] primes) {
+      if (primes.length > 0) {
+         reverse(primes);
+         System.out.print(primes[0] + "  ");
+         reverse(primes);
+         
+         int [] newPrimes = new int [primes.length-1];
+         for (int i = 0; i < primes.length-1; i++)
+            newPrimes[i] = primes[i];
+         
+         backwardDisplay(newPrimes);
       }
    }
 
+   private static void reverse(int [] toReverse) {
+      for (int i = 0; i < toReverse.length/2; i++) {
+         int tmp = toReverse[i];
+         toReverse[i] = toReverse[toReverse.length-1-i];
+         toReverse[toReverse.length-1-i] = tmp;
+      }
+   }
 }
