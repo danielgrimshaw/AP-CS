471cab5d943a6f95f81c815ac5da88f4ec854ec1
Daniel Grimshaw <danielgrimshaw42@gmail.com>
2015-03-12 17:57:23 -0700

    Finished with selection sort
    Such a slow algorithm!

--- Classwork/src/GrimshawSelection.java
+++ Classwork/src/GrimshawSelection.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
 
-public class GrimshawMSelection {
+public class GrimshawSelection {
    public static void main(String [] args) {
       int [] array1 = {22, 18, 12, -4, 27, 30, 36, 50, 7, 68, 91, 56, 2, 85, 42, 98, 25};
       int [] array2 = {55};
       int [] array3 = {98, 88, 76, 54, 45, 36, 32, 31, 31, 25, 19, 17, 12, 7, 5, 0, -10};
+      int [] array4 = {22, 18, 12, -4, 27, 30, 36, 50, 7, 68, 91, 56, 2, 85, 42, 98, 25};
+      int [] array5 = {55};
+      int [] array6 = {98, 88, 76, 54, 45, 36, 32, 31, 31, 25, 19, 17, 12, 7, 5, 0, -10};
       
       reverseSelectionSort(array1);
       reverseSelectionSort(array2);
@@ -14,13 +17,13 @@ public class GrimshawMSelection {
       System.out.println(Arrays.toString(array2));
       System.out.println(Arrays.toString(array3));
       
-      dualSelectionSort(array1);
-      dualSelectionSort(array2);
-      dualSelectionSort(array3);
+      dualSelectionSort(array4);
+      dualSelectionSort(array5);
+      dualSelectionSort(array6);
       
-      System.out.println(Arrays.toString(array1));
-      System.out.println(Arrays.toString(array2));
-      System.out.println(Arrays.toString(array3));
+      System.out.println(Arrays.toString(array4));
+      System.out.println(Arrays.toString(array5));
+      System.out.println(Arrays.toString(array6));
    }
    
    public static void reverseSelectionSort(int [] a) {
@@ -41,22 +44,23 @@ public class GrimshawMSelection {
       int maxPos;
       int minPos;
    
-      for (int j = 0; j < a.length-1; j++) {
-         maxPos = j;
+      for (int j = 0; j < a.length/2-1; j++) {
+         maxPos = j+1;
          minPos = j;
          
-         for (int i = j+1; i < a.length; i++) {
-            if (a[i] > a[maxPos])
-               maxPos = i;
+         for (int i = j+1; i < a.length-j; i++)
             if (a[i] < a[minPos])
                minPos = i;
-         }
-         
+               
          int tmp = a[j];
          a[j] = a[minPos];
          a[minPos] = tmp;
          
-         int tmp2 = a[a.length-1-j];
+         for (int i = j+2; i < a.length-j; i++)
+            if (a[i] > a[maxPos])
+               maxPos = i;
+         
+         tmp = a[a.length-1-j];
          a[a.length-1-j] = a[maxPos];
          a[maxPos] = tmp;
       }
